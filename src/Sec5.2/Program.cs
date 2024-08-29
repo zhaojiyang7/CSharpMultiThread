a541883d568f02a41e3a2fe0466e2eaf30b8faa2
jiyang.zhao <jiyang.zhao@united-imaging.com>
2024-08-29 16:33:01 +0800

    第五章（1-4）

---
+++ src/Sec5.2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using static System.Threading.Thread;
+namespace Sec5._2
+{
+    internal class Program
+    {
+        static Task AsynchronyWithTPL()
+        {
+            Task<string> t = GetInfoAsync("Task1");
+            Task t2 = t.ContinueWith(task => Console.WriteLine(t.Result), TaskContinuationOptions.NotOnFaulted);
+            Task t3 = t.ContinueWith(task => Console.WriteLine(t.Exception.InnerException), TaskContinuationOptions.OnlyOnFaulted);
+            return Task.WhenAny(t2, t3);
+        }
+        static async Task AsynchronoyWithAwait()
+        {
+            try
+            {
+                string result = await GetInfoAsync("task2");
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+        static async Task<string> GetInfoAsync(string name)
+        {
+            await Task.Delay(2000);
+            throw new Exception("boom");
+            return $"task {name} is {CurrentThread.ManagedThreadId}";
+        }
+        static void Main(string[] args)
+        {
+            Task t1=AsynchronyWithTPL();
+            t1.Wait();
+            Task t2 = AsynchronoyWithAwait();
+            t2.Wait();
+        }
+    }
+}
---
+++ src/Sec5.3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using static System.Threading.Thread;
+namespace Sec5._3
+{
+    internal class Program
+    {
+        static async Task AsyncProcessing()
+        {
+            Func<string,Task<string>> asyncLambda = async name => {
+                await Task.Delay(2000);
+                return $"task {name} running on {CurrentThread.ManagedThreadId}";
+            };
+            string result = await asyncLambda("zjy");
+            Console.WriteLine(result);
+        }
+        static async Task<string> GetInfoAsync(string name)
+        {
+            Console.WriteLine($"Task {name} started");
+            await Task.Delay(2000);
+            return $"task {name} is runnning";
+        }
+        static async Task Test()
+        {
+            var res = await GetInfoAsync("niuniuniu");
+            Console.WriteLine(res);
+            res = await GetInfoAsync("zjy");
+            Console.WriteLine(res);
+        }
+        static void Main(string[] args)
+        {
+            /*            var t = AsyncProcessing();
+                        t.Wait();*/
+            var t = Test();
+            t.Wait();
+        }
+    }
+}
